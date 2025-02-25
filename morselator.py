aa10469d795aa33b0f1cdae671e3b6c55636e174
grishasolakhyan <borsch.tv@mail.ru>
2025-02-25 22:46:52 +1000

    Update Morselator 0.1.3

--- morselator.py
+++ morselator.py
@@ -1,26 +1,52 @@
 my_dict = {
-    '1': '.----',
-    '2': '..---',
-    '3': '...--',
-    '4': '....-',
-    '5': '.....',
-    '6': '-....',
-    '7': '--...',
-    '8': '---..',
-    '9': '----.',
-    '0': '-----',
+    'A':	'.-',
+    'B':	'-...',
+    'C':	'-.-.',
+    'D':	'-..',
+    'E':	'.',
+    'F':	'..-.',
+    'G':	'--.',
+    'H':	'....',
+    'I':	'..',
+    'J':	'.---',
+    'K':	'-.-',
+    'L':	'.-..',
+    'M':	'--',
+    'N':	'-.',
+    'O':	'---',
+    'P':	'.--.',
+    'Q':	'--.-',
+    'R':	'.-.',
+    'S':	'...',
+    'T':	'-',
+    'U':	'..-',
+    'V':	'...-',
+    'W':	'.--',
+    'X':	'-..-',
+    'Y':	'-.--',
+    'Z':	'--..',
+    '0':	'-----',
+    '1':	'.----',
+    '2':	'..---',
+    '3':	'...--',
+    '4':	'....-',
+    '5':	'.....',
+    '6':	'-....',
+    '7':	'--...',
+    '8':	'---..',
+    '9':	'----.',
     ' ': ' '
 }
-str_number = str(input(f'Enter the number: '))
-print(f'Number is {str_number}')
-len_str_number = len(str_number)
+orig_line = str(input(f'Enter the number: '))
+print(f'Original line: {orig_line}')
 
-def morse_translator(dict):
+def morse_translator(orig_str, dict):
+    len_str_number = len(orig_str)
     morse_string = ''
     for i in range(len_str_number):
-        if str_number[i] in dict:
-            symb = dict[str_number[i]]
+        if orig_str[i] in dict:
+            symb = dict[orig_str[i]]
             morse_string += symb
     return morse_string
 
-print(f'Morse line: {morse_translator(my_dict)}')
+print(f'Morse line: {morse_translator(orig_line, my_dict)}')
